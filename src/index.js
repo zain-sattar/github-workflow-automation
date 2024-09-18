3be9dcd4380f9956b0a97bf87c368f779dff7e81
zain-sattar <zain.sattar@mergestack.com>
2024-09-18 19:30:03 +0500

    test sync-develop workflow on push to main branch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 console.log("Hello world");
+console.log(
+  "This repository demonstrates an automated workflow using GitHub Actions to keep the develop branch in sync with the main branch. Whenever changes are pushed to the main branch (such as hotfixes or updates), the workflow will automatically merge these changes into the develop branch, ensuring both branches are always up-to-date."
+);
